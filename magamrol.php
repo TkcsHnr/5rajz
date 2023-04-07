6777632c62e393d01062907409c772b7c6ffb56b
Takacs Hunor <tkcshnr@gmail.com>
2023-04-07 02:03:30 +0200

    links open on new page

--- magamrol.php
+++ magamrol.php
@@ -52,7 +52,7 @@
             <div>
                 <h2>Fontosabb publikációk és előadások:</h2>
                 <div class="publikaciok">
-                    <div><a href="https://www.eltereader.hu/media/2020/10/Endr%C5%91dy-%E2%80%93-Svraka-%E2%80%93-F-Lass%C3%BA-Soksz%C3%ADn%C5%B1-pedag%C3%B3gia-WEB.pdf">
+                    <div><a href="https://www.eltereader.hu/media/2020/10/Endr%C5%91dy-%E2%80%93-Svraka-%E2%80%93-F-Lass%C3%BA-Soksz%C3%ADn%C5%B1-pedag%C3%B3gia-WEB.pdf" target="_blank">
                             <img src="resources/click.png" alt="" class="link">
                         </a>
                         <p>
@@ -61,36 +61,36 @@
                         </p>
                     </div>
 
-                    <div><a href="https://www.youtube.com/watch?v=IR9ii5TvQmM"> <img src="resources/click.png" alt="" class="link">
+                    <div><a href="https://www.youtube.com/watch?v=IR9ii5TvQmM" target="_blank"> <img src="resources/click.png" alt="" class="link">
                         </a>
                         <p>VIMM27: Művészetterápia és vizuális nevelés, Vizuális Mesterpedagógus Műhely Deák17 Gyermek és Ifjúsági Művészeti Galéria, 2019. szeptember 21. Workshop az 5-szimbólum önkifejező feladatsorról (előadás)
                         </p>
                     </div>
-                    <div><a href="https://epa.oszk.hu/02400/02411/00010/pdf/"> <img src="resources/click.png" alt="" class="link">
+                    <div><a href="https://epa.oszk.hu/02400/02411/00010/pdf/" target="_blank"> <img src="resources/click.png" alt="" class="link">
                             <p>
                         </a>Szimbólumok rajzai, rajzok szimbólumai. Szimbólumrajzok pedagógiai használatának lehetőségei kamaszkorban in: Gyermeknevelés Online tudományos folyóirat, A gyermekkortörténeti ikonográfia kiáltványa, 2017/1. p. 155- 170.
                         </p>
                     </div>
 
-                    <div><a href=" https://mek.oszk.hu/18600/18644/18644.pdf"> <img src="resources/click.png" alt="" class="link">
+                    <div><a href=" https://mek.oszk.hu/18600/18644/18644.pdf" target="_blank"> <img src="resources/click.png" alt="" class="link">
                             <p>
                         </a>„Kiutat kereső fény” – kamaszrajzok üzenete az 5-szimbólum feladatsorral in: Nagy Ádám (szerk.): Szolgálva, nem tündökölve. Iuvenis Ifjúságszakmai Műhely, ISZT Alapítvány, Budapest, 2017. p. 260-278.
                         </p>
                     </div>
 
-                    <div><a href="https://mptpszichiatria.hu/eredmeny.aspx?web_id=&tmi=0&lang=hun"> <img src="resources/click.png" alt="" class="link">
+                    <div><a href="https://mptpszichiatria.hu/eredmeny.aspx?web_id=&tmi=0&lang=hun" target="_blank"> <img src="resources/click.png" alt="" class="link">
                         </a>
                         <p>A média elemeinek hatása 14-16 éves serdülők önkifejező rajzain in: PSYCHIATRIA HUNGARICA 30:(2) pp. 192-200. (2015)
                         </p>
                     </div>
 
-                    <div><a href=" https://mptpszichiatria.hu/folyoirat.aspx?web_id=&tmi=0&f=1&an=1259#j"> <img src="resources/click.png" alt="" class="link">
+                    <div><a href=" https://mptpszichiatria.hu/folyoirat.aspx?web_id=&tmi=0&f=1&an=1259#j" target="_blank"> <img src="resources/click.png" alt="" class="link">
                         </a>
                         <p>Az agresszió képi jegyei 14-16 éves serdülők rajzain in: PSYCHIATRIA HUNGARICA XXVIII.:(1) pp. 57-69. (2013)
                         </p>
                     </div>
 
-                    <div><a href="https://www.youtube.com/watch?v=rBC6ErgQRyg"> <img src="resources/click.png" alt="" class="link">
+                    <div><a href="https://www.youtube.com/watch?v=rBC6ErgQRyg" target="_blank"> <img src="resources/click.png" alt="" class="link">
                         </a>
                         <p>Az 5-szimbólum rajzi feladatsor eredményességének vizsgálata (előadás) XIII. Országos Neveléstudományi Konferencia, Eger
                         </p>
