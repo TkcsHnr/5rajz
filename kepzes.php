a6868eb17d30925213886954155f8961426e092f
Takacs Hunor <tkcshnr@gmail.com>
2023-04-10 13:12:45 +0200

    Képzés page crated

---
+++ kepzes.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Képzés</title>
+
+    <?php include('includes/links.php') ?>
+    <link rel="stylesheet" href="css/leirasok/leiras.css">
+</head>
+
+<body>
+    <div class="bg">
+        <header>
+            <?php include 'includes/header.php' ?>
+        </header>
+
+        <script>
+            document.getElementById('kepzes').classList.add('active');
+        </script>
+
+        <main>
+            <h2>
+                Bevezetés a rajzelemzés pedagógiai, nem terápiás célú felhasználásába
+                <br>(kiscsoportos gyakorlati képzés)
+            </h2>
+
+            <div>
+                <h2>A képzés célja és menete:</h2>
+                <p>
+                    A csomag <span>ötvözi az elméleti és gyakorlati elemeket, így kiválóan alkalmas a pedagógiai eszköztár bővítésére. A képzés tartalmazza az 5-szimbólum feladatsor saját élményű megrajzolását</span>, így az elméleti háttér lépései mentén a résztvevők a saját rajzaikat valamint egymás munkáit elemzik, és dolgozzák fel. <span>Minden témakörhöz kapcsolódik gyakorlati feladat és önálló munka</span>, melyet közösen megbeszélünk. A tematika időbeosztása nem szigorúan kötött, a résztvevők kérdései és érdeklődése alapján, vagy egyéni igények szerint alakítható. <span>A képzést kiegészíti az egyéni munka során felvett rajzokról való konzultáció</span>, mely elősegíti a gyakorlati tudás elmélyítését. <span>Szakirodalmat témakörönként ajánlok</span> az alaposabb megismerés érdekében.
+                </p>
+                <p>
+                    A képzés előre egyeztetett időpontban, <span>kiscsoportos formában</span> történik. A gyakorlati feladatok miatt <span>ajánlott a páros csoportlétszám</span>. Külön árajánlat érvényes a <span>2-6 fős és a 8-10 fős csoportok</span> esetén. <span>A gyakorlati feladatokhoz szükséges eszközöket biztosítom</span> (rajzlap, grafitceruza, színesceruza). A <span>témakörök egymásra épülnek</span>, a következő lépcsőfok értő használatához szükség van az előző tudásanyagra.
+                </p>
+                <p>
+                    <span>A képzés 5 alkalomból áll, alkalmanként 3 óra</span> (2x1,5 óra, közötte rövid szünet). Minden alkalom tartalmaz <span>elméleti és gyakorlati</span> részt is.
+                </p>
+            </div>
+            <div class="divcenter">
+                <h2>Témakörök: (részletes tematikát a jelentkezés után kapnak a résztvevők)</h2>
+                <div class="list">
+                    <p><span>1.</span> Projektív rajz fogalma és az iskolai használat lehetőségei</p>
+                    <p><span>2.</span> A rajzfelvétel gyakorlata</p> 
+                    <p><span>3.</span> Tartalmi rajzelemzés</p> 
+                    <p><span>4.</span> Formai rajzelemzés</p> 
+                    <p><span>5.</span> Az 5-szimbólum feladatsor elméleti háttere, értelmezése, és gyakorlati használata</p>
+                </div>
+            </div>
+            <div>
+                <h2>Kinek ajánlom a képzést?</h2>
+                <p>A képzés <span>a projektív rajzok és a rajzelemzés pedagógiai használatát</span>, elemzését és lehetőségeit állítja az előtérbe, ezért <span>elsősorban az iskolai munkához kapcsolódó szakembereknek ajánlom</span>. Jó lehetőség <span>tanítóknak és tanároknak, osztályfőnököknek, rajztanároknak, napközis és szakköri nevelőknek, iskolapszichológusoknak, családsegítő és gyermekvédelmi szakembereknek, védőnőknek és mentor-tanároknak</span>. A képzés emellett nagyon jó gyakorlati alapozást kínál <span>egyéb segítő szakmában dolgozó munkatársaknak is, (pl. mentálhigiénés munkatársak, lelki gondozók, coach-ok, mediátorok</span>, stb.) vagy bárki más érdeklődő számára. </p>
+            </div>
+
+            <p class="underline">
+                <span>A képzés alapozó jellegű. Az 5-szimbólum feladatsort és a rajzelemzés gyakorlatát mindenki a saját kompetenciáinak megfelelően, saját szakmai hatáskörén belül használhatja.</span>
+            </p>
+        </main>
+
+        <footer>
+            <?php include 'includes/footer.php' ?>
+        </footer>
+    </div>
+</body>
+
+</html>
